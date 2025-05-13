9c94482188693d351009626d48da06906b3a0819
Manuel Ricardo Vargas Cruz <manuelricardovargascruz@gmail.com>
2025-05-12 21:27:02 -0500

    mapas

---
+++ scripts/02_Extracción_datos_espaciales.R
@@ -0,0 +1,26 @@
+# =========================================================
+# 0. Workspace configuration
+# =========================================================
+
+# Clear workspace
+
+rm(list = ls())
+
+# Set up paths
+
+dir <- list ()
+dir$root <- getwd()
+dir$processed <- file.path(dir$root, "stores", "processed")
+dir$raw <- file.path(dir$root, "stores", "raw")
+dir$views <- file.path(dir$root, "views")
+dir$scripts <- file.path(dir$root, "scripts")
+setwd(dir$root)
+
+# Load required libraries
+
+source(file.path(dir$scripts, "00_load_requirements.R"))
+
+# Load inputs
+
+train <- read.csv(file.path(dir$raw, "train.csv"))
+test <- read.csv(file.path(dir$raw, "test.csv"))
